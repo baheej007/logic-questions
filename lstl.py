744273036dc41fd4a37180ff01e76cb75c6eaf73
baheej007 <139341762+baheej007@users.noreply.github.com>
2025-01-02 23:24:53 +0530

    Update

---
+++ lstl.py
@@ -0,0 +1,14 @@
+def merge_sorted_lists(l1, l2):
+    i, j, merged = 0, 0, []
+    while i < len(l1) and j < len(l2):
+        if l1[i] < l2[j]:
+            merged.append(l1[i])
+            i += 1
+        else:
+            merged.append(l2[j])
+            j += 1
+    merged.extend(l1[i:])
+    merged.extend(l2[j:])
+    return merged
+
+print(merge_sorted_lists([1, 3, 5], [2, 4, 6]))
