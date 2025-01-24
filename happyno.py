76b9f672c2e3d72aaba85282575973e1dc34f2d0
baheej007 <139341762+baheej007@users.noreply.github.com>
2025-01-24 23:52:04 +0530

    Update

---
+++ happyno.py
@@ -0,0 +1,8 @@
+def is_happy_number(num):
+    seen = set()
+    while num != 1 and num not in seen:
+        seen.add(num)
+        num = sum(int(digit) ** 2 for digit in str(num))
+    return num == 1
+
+print(is_happy_number(19))
