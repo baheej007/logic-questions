cd3dfd73d11fed848627186d06170e086e054a5a
baheej007 <139341762+baheej007@users.noreply.github.com>
2025-01-27 22:03:34 +0530

    Create simpLRU.py

---
+++ simpLRU.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        elif len(self.cache) >= self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+
+cache = LRUCache(2)
+cache.put(1, 1)
+cache.put(2, 2)
+print(cache.get(1))
+cache.put(3, 3)
+print(cache.get(2))
