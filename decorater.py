91206206ebdf57e41a7649f35c0e11adae96efac
baheejmhd <139341762+baheej007@users.noreply.github.com>
2025-05-23 21:12:55 +0530

    Create decorater.py

---
+++ decorater.py
@@ -0,0 +1,19 @@
+import time
+from functools import wraps
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} executed in {end - start:.4f}s")
+        return result
+    return wrapper
+
+@timer
+def slow_function():
+    time.sleep(2)
+    return "Finished!"
+
+print(slow_function())
