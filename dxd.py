831ed09a19e227e6d762f6925a879b0eaaa4a477
baheejmhd <139341762+baheej007@users.noreply.github.com>
2025-06-01 15:59:40 +0530

    Create dxd.py

---
+++ dxd.py
@@ -0,0 +1,7 @@
+import string, random
+
+def generate_pwd(length=12):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*()"
+    return ''.join(random.choice(chars) for _ in range(length))
+
+print(generate_pwd())
