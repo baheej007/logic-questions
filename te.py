926bfadf5335a0d9f8bf032ad81b32939124db55
baheejmhd <139341762+baheej007@users.noreply.github.com>
2025-07-03 18:00:20 +0530

    Create te.py

---
+++ te.py
@@ -0,0 +1,12 @@
+try:
+    # Use "raise" to raise an error
+    raise IndexError("This is an index error")
+except IndexError as e:
+    pass                 # Refrain from this, provide a recovery (next example).
+except (TypeError, NameError):
+    pass                 # Multiple exceptions can be processed jointly.
+else:                    # Optional clause to the try/except block. Must follow
+                         # all except blocks.
+    print("All good!")   # Runs only if the code in try raises no exceptions
+finally:                 # Execute under all circumstances
+    print("We can clean up resources here")
