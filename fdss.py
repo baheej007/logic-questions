d6ff777b0348f887a6ba315d9f8c3473ffd6d165
baheejmhd <139341762+baheej007@users.noreply.github.com>
2025-07-05 18:36:33 +0530

    Create fdss.py

---
+++ fdss.py
@@ -0,0 +1,9 @@
+# 4. Conditional statements (if-elif-else)
+if age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
+# 5. Loops (for loop and while loop)
