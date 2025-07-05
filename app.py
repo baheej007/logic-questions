500c4aabdee94a469146c273177a7cc698b0ffd0
baheejmhd <139341762+baheej007@users.noreply.github.com>
2025-07-05 18:35:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+print(f"\nFruits list: {fruits}")
+print(f"First fruit: {fruits[0]}")
+fruits.append("orange") # Add an element
+print(f"Fruits after adding: {fruits}")
