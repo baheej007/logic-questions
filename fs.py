e5d9dec19830924520c3ea173ae59d801761c2c6
baheejmhd <139341762+baheej007@users.noreply.github.com>
2025-07-05 18:35:15 +0530

    Create fs.py

---
+++ fs.py
@@ -0,0 +1,9 @@
+# 3. Basic arithmetic operations
+num1 = 10
+num2 = 5
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+print(f"Floor Division: {num1 // num2}") # Returns the integer part of the division
+print(f"Modulo: {num1 % num2}") # 
